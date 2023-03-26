01aaf9cd36563ea98a80b7ffe1a35880ee17a8cc
AsmaMohame <117165982+AsmaMohame@users.noreply.github.com>
2023-03-26 17:21:14 +0200

    Add files via upload

---
+++ scanner/Scanner - Copy/projects/ngx-web-document-scanner/src/lib/ngx-web-document-scanner.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-web-document-scanner',
+  template: `
+    <p>
+      ngx-web-document-scanner works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NgxWebDocumentScannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ scanner/Scanner - Copy/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { DwtComponent } from './dwt/dwt.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DwtComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
