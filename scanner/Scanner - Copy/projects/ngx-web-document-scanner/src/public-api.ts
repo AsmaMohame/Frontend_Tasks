91369e40b8498803c476d828cde0780ba1176256
AsmaMohame <117165982+AsmaMohame@users.noreply.github.com>
2023-03-27 10:42:25 +0200

    Delete scanner/Scanner - Copy directory

--- scanner/Scanner - Copy/projects/ngx-web-document-scanner/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-web-document-scanner
- */
-
-export * from './lib/ngx-web-document-scanner.service';
-export * from './lib/ngx-web-document-scanner.component';
-export * from './lib/ngx-web-document-scanner.module';
